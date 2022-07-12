64c2e3a74859fbf1457833d911a74c11a480770b
Jonnix12 <jonharel48@gmail.com>
2022-07-12 20:43:52 +0300

    daniel like boys

--- Assets/Scrips/Car/MiniMap.cs
+++ Assets/Scrips/Car/MiniMap.cs
@@ -4,30 +4,12 @@ using UnityEngine;
 
 public class MiniMap : MonoBehaviour
 {
-    [SerializeField] private CheckPointSystem _checkPointSystem;
-    private LineRenderer lineRenderer;
-
     public GameObject car;
     public GameObject miniMapCam;
     public GameObject Player;
 
    
     // Start is called before the first frame update
-    void Start()
-    {
-        lineRenderer = GetComponent<LineRenderer>();
-        
-       
-        for (int x = 0; x <_checkPointSystem.CheckPointPosition.Count; x++)
-        {
-            lineRenderer.SetPosition(x,new Vector3(_checkPointSystem.CheckPointPosition[x].x,60,_checkPointSystem.CheckPointPosition[x].z));
-        }
-        lineRenderer.SetPosition(_checkPointSystem.CheckPointPosition.Count, lineRenderer.GetPosition(0));
-         
-        lineRenderer.startWidth = 9f;
-        lineRenderer.endWidth = 9f;
-    }
-
     private void Update()
     {
         miniMapCam.transform.position = (new Vector3(car.transform.position.x, miniMapCam.transform.position.y, car.transform.position.z));
